2bf10de0b896883912b8d2fb3130c4ce6738fc6c
hassydhw <g051tg2017@dhw.ac.jp>
2018-12-26 06:19:06 +0900

    commit with existing 9 scenes

---
+++ Assets/_sampleAssets/ButtonInputManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInputManager : MonoBehaviour
+{
+
+    public TextMesh buttonInputMesh;
+    public TextMesh lastButtonMesh;
+    public TextMesh padInfoMesh;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //トリガー、タッチパッドボタンを押し続けているかどうか
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            buttonInputMesh.text = "trigger pressing";
+        }
+        else if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        {
+            buttonInputMesh.text = "pad pressing";
+        }
+        else
+        {
+            buttonInputMesh.text = "nothing pressed";
+        }
+
+
+
+        //トリガー、タッチパッドボタン、バックボタンを押したかどうか
+        if (OVRInput.GetDown(OVRInput.Button.Back))
+        {
+            lastButtonMesh.text = "backbutton pressed";
+        }
+        else if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            lastButtonMesh.text = "trigger pressed";
+        }
+        else if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            lastButtonMesh.text = "pad pressed";
+        }
+
+
+        //タッチパッドにさわったばあいはその位置を表示、パッドの中心が0,0
+        if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad))
+        {
+            //            padInfoMesh.text = "pad touching";
+            Vector2 touchPadPt = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+            padInfoMesh.text = touchPadPt.ToString();
+        }
+        else
+        {
+            padInfoMesh.text = "pad NOT TOUCH";
+        }
+
+
+
+
+    }
+}
