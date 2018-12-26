53cfe7a7984c5c12d903cfcdb3c3f550e75afbf6
hassydhw <g051tg2017@dhw.ac.jp>
2018-12-26 21:08:48 +0900

    feat controller acceleration

---
+++ Assets/_sampleAssets/ControllerRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerRotation : MonoBehaviour
+{
+
+    public TextMesh rotationMesh;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Controller activeController = OVRInput.GetActiveController();
+
+        Quaternion rot = OVRInput.GetLocalControllerRotation(activeController);
+        transform.rotation = rot;
+        rotationMesh.text = rot.eulerAngles.ToString();
+
+    }
+}
