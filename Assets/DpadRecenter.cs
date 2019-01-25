25288132bbb7311936ea507686f9eaef53eae051
hassydhw <g051tg2017@dhw.ac.jp>
2019-01-26 07:00:36 +0900

    add scene13 recenter

---
+++ Assets/DpadRecenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.XR;
+
+
+public class DpadRecenter : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            OVRManager.display.RecenterPose();
+            //            InputTracking.Recenter();// これでもOK、ただしusing必要
+        }
+    }
+}
